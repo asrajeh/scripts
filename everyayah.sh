fd16c5138ad8e3444341d5a1a0982ca155f29793
Abdullah Alrajeh <asrajeh@gmail.com>
2018-03-15 10:46:04 +0300

    Add files via upload

---
+++ everyayah.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# A script to download from everyayah.com
+# written by Abdullah Alrajeh
+# ./everyayah.sh <ayah> <surah>
+# For example: ./everyayah.sh 001 001
+
+if [ "$#" -ne 2 ]; then
+  echo "./everyayah.sh <ayah> <surah>"
+  exit 1
+fi
+
+mkdir -p quran
+cd quran
+
+if [ ! -f readrs ]; then
+  wget http://everyayah.com/data/status.php
+  cat status.php|sed "s/^[ \t]*//"|grep 'href="/data/[A-Za-z_0-9-]*/" target="_blank">GO' | cut -d'/' -f3 > readers
+fi
+
+# recited by all readers
+
+R=$(cat readers)
+
+for r in $R
+do
+  mkdir -p $r
+  cd $r
+  wget http://www.everyayah.com/data/$r/$1$2.mp3
+  cd ..
+done
+
