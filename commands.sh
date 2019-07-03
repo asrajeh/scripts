ab081dc0407d67b2ce32581490b5573f5162093c
Abdullah Alrajeh <asrajeh@gmail.com>
2019-07-03 10:06:38 +0300

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,2 +1,5 @@
 # extract the first paper in a pdf file as an image
 convert -density 100 paper.pdf[0] - | convert -resize 56 - paper.jpg
+
+# split audio on silence
+sox in.wav out.wav silence 1 0.1 0.1% 1 0.1 0.1% : newfile : restart
