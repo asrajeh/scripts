08765f43225e4aaa58119e2f931eb69e39104b7f
Abdullah Alrajeh <asrajeh@gmail.com>
2019-07-04 19:57:39 +0300

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -3,3 +3,6 @@ convert -density 100 paper.pdf[0] - | convert -resize 56 - paper.jpg
 
 # split audio on silence
 sox in.wav out.wav silence 1 0.1 0.1% 1 0.1 0.1% : newfile : restart
+
+# print lines between two patterns
+awk '/pattern1/ {p=1}; p; /pattern2/ {p=0}' file
