bb1d462e41420422da4fcd3ecee77cc6a5065719
Abdullah Alrajeh <asrajeh@gmail.com>
2019-11-06 07:14:51 +0300

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -15,6 +15,27 @@ for f in *.raw; do sox -r 16000 -e signed -b 16 -c 1 $f ${f%.raw}.wav; done
 # print lines between two patterns
 awk '/pattern1/ {p=1}; p; /pattern2/ {p=0}' file
 
+# save it in .bashrc or .profile
+extract () {
+    if [ -f $1 ] ; then
+        case $1 in
+            *.tar.bz2)  tar xjf $1      ;;
+            *.tar.gz)   tar xzf $1      ;;
+            *.bz2)      bunzip2 $1      ;;
+            *.rar)      rar x $1        ;;
+            *.gz)       gunzip $1       ;;
+            *.tar)      tar xf $1       ;;
+            *.tbz2)     tar xjf $1      ;;
+            *.tgz)      tar xzf $1      ;;
+            *.zip)      unzip $1        ;;
+            *.Z)        uncompress $1   ;;
+            *)          echo "'$1' cannot be extracted via extract()" ;;
+        esac
+    else
+        echo "'$1' is not a valid file"
+    fi
+}
+
 # one-liner that converts Arabic to Buckwalter
 echo 'السلام عليكم' | perl -CSD -Mutf8 -npe "BEGIN {use IO::Handle; STDOUT->autoflush(1);} tr/\N{U+0621}\N{U+0622}\N{U+0623}\N{U+0624}\N{U+0625}\N{U+0626}\N{U+0627}\N{U+0628}\N{U+0629}\N{U+062A}\N{U+062B}\N{U+062C}\N{U+062D}\N{U+062E}\N{U+062F}\N{U+0630}\N{U+0631}\N{U+0632}\N{U+0633}\N{U+0634}\N{U+0635}\N{U+0636}\N{U+0637}\N{U+0638}\N{U+0639}\N{U+063A}\N{U+0640}\N{U+0641}\N{U+0642}\N{U+0643}\N{U+0644}\N{U+0645}\N{U+0646}\N{U+0647}\N{U+0648}\N{U+0649}\N{U+064A}\N{U+064B}\N{U+064C}\N{U+064D}\N{U+064E}\N{U+064F}\N{U+0650}\N{U+0651}\N{U+0652}\N{U+0670}\N{U+0671}/'|>&<}AbptvjHxd*rzs\$SDTZEg_fqklmnhwYyFNKaui~o\`{/;"
 
