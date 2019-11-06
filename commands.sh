1fd70cc099a3ace2f83f567df26ed219f260728e
Abdullah Alrajeh <asrajeh@gmail.com>
2019-11-06 07:18:37 +0300

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -19,14 +19,14 @@ awk '/pattern1/ {p=1}; p; /pattern2/ {p=0}' file
 extract () {
     if [ -f $1 ] ; then
         case $1 in
-            *.tar.bz2)  tar xjf $1      ;;
-            *.tar.gz)   tar xzf $1      ;;
+            *.tar.bz2)  tar vxjf $1      ;;
+            *.tar.gz)   tar vxzf $1      ;;
             *.bz2)      bunzip2 $1      ;;
             *.rar)      rar x $1        ;;
             *.gz)       gunzip $1       ;;
-            *.tar)      tar xf $1       ;;
-            *.tbz2)     tar xjf $1      ;;
-            *.tgz)      tar xzf $1      ;;
+            *.tar)      tar vxf $1       ;;
+            *.tbz2)     tar vxjf $1      ;;
+            *.tgz)      tar vxzf $1      ;;
             *.zip)      unzip $1        ;;
             *.Z)        uncompress $1   ;;
             *)          echo "'$1' cannot be extracted via extract()" ;;
